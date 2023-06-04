ce5a63e178ef29fc13a53e68fe83e8825e998ae0
ZokirjonovaMuslima <muslimazokirjonova2004@gmail.com>
2023-06-05 00:56:14 +0500

    update

--- apps/views.py
+++ apps/views.py
@@ -1,5 +1,7 @@
 # Product
+from django.core.cache import cache
 from rest_framework.generics import RetrieveAPIView, ListCreateAPIView, CreateAPIView
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from apps.models import Product, Category, Wishlist, Order, Basket
@@ -10,6 +12,13 @@ from apps.serializers import ProductModelSerializer, CategoryModelSerializer, Wi
 class ProductModelViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductModelSerializer
+
+    def list(self, request, *args, **kwargs):
+        if cache.get('data') is None:
+            cache.set('data', self.get_queryset(), timeout=60)
+            return Response(self.get_serializer(self.get_queryset(), many=True).data)
+        else:
+            return Response(self.get_serializer(cache.get('data'), many=True).data)
 
 
 class ProductDetailRetrieveAPIView(RetrieveAPIView):
